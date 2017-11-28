ea4560cb3588bb0c459ce9f2b323d44855e63fba
Michael Birtwell <michael.birtwell@gmail.com>
2017-11-28 08:33:27 +0000

    Rename to remove calculator reference

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@ extern crate lalrpop_util;
 
 pub mod ast;
 
-pub mod calculator5;
+pub mod grammar;
 
 #[test]
 fn calculator5() {
-    assert_eq!(&format!("{:?}", calculator5::parse_Exprs("").unwrap()),
+    assert_eq!(&format!("{:?}", grammar::parse_Exprs("").unwrap()),
                "[]");
-    assert_eq!(&format!("{:?}", calculator5::parse_Exprs("22 * 44 + 66").unwrap()),
+    assert_eq!(&format!("{:?}", grammar::parse_Exprs("22 * 44 + 66").unwrap()),
                "[((22 * 44) + 66)]");
-    assert_eq!(&format!("{:?}", calculator5::parse_Exprs("22 * 44 + 66,").unwrap()),
+    assert_eq!(&format!("{:?}", grammar::parse_Exprs("22 * 44 + 66,").unwrap()),
                "[((22 * 44) + 66)]");
-    assert_eq!(&format!("{:?}", calculator5::parse_Exprs("22 * 44 + 66, 13*3").unwrap()),
+    assert_eq!(&format!("{:?}", grammar::parse_Exprs("22 * 44 + 66, 13*3").unwrap()),
                "[((22 * 44) + 66), (13 * 3)]");
-    assert_eq!(&format!("{:?}", calculator5::parse_Exprs("22 * 44 + 66, 13*3,").unwrap()),
+    assert_eq!(&format!("{:?}", grammar::parse_Exprs("22 * 44 + 66, 13*3,").unwrap()),
                "[((22 * 44) + 66), (13 * 3)]");
 }
 
